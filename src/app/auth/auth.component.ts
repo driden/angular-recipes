0c8b79bbb223f87435a57bb207b4eab617424648
Ignacio Loureiro <Ignacio.Loureiro@endava.com>
2020-01-28 17:58:52 -0300

    auth component

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html'
+})
+export class AuthComponent {
+  isLoginMode = false;
+
+  switchMode = () => (this.isLoginMode = !this.isLoginMode);
+
+  onSubmit(authForm: NgForm) {
+    console.log(authForm);
+    authForm.reset();
+  }
+}
