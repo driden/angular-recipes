a746d395990a2c70f9cbe3cc45aae83770aacbb3
driden <776982+driden@users.noreply.github.com>
2020-02-17 11:45:31 -0300

    shoppinglist & recipes modules lazily loaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -7,11 +7,24 @@ const routes: Routes = [
     path: 'recipes',
     loadChildren: () =>
       import('./recipes/recipes.module').then(m => m.RecipesModule)
+  },
+  {
+    path: 'shopping-list',
+    loadChildren: () =>
+      import('./shopping-list/shopping-list.module').then(
+        m => m.ShoppingListModule
+      )
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
