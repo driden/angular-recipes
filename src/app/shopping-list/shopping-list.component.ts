f5300e5ffa873b75dcd0c3598c3e025acfece7c3
driden <776982+driden@users.noreply.github.com>
2020-02-17 12:15:27 -0300

    small refactor to work with AoT

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,17 +1,21 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
-import { Ingredient } from "../shared/Ingredient";
-import { ShoppingListService } from "./shopping-list.service";
+import { Ingredient } from '../shared/Ingredient';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
-  selector: "app-shopping-list",
-  templateUrl: "./shopping-list.component.html",
-  styleUrls: ["./shopping-list.component.css"]
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
   constructor(private shopListSvc: ShoppingListService) {}
 
   onIngredientClicked(clickedIngredient: Ingredient) {
     this.shopListSvc.editingIngredient.next(clickedIngredient);
+  }
+
+  get ingredients(): Ingredient[] {
+    return this.shopListSvc.getAll();
   }
 }
